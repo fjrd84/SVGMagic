68c553e63b28a2e23ff166bf57ce1c892202d411
F. Javier R. Donado <fjrd84@gmail.com>
2015-06-21 20:19:04 +0200

    Updated colors

--- js/svgm.js
+++ js/svgm.js
@@ -1,49 +1,48 @@
-var svgm = (function(){
+/* global $*/
+// Namespace structure
+var svgm = (function () {
     'use strict';
     return {
-        startMagic: function(){}
+        startMagic: function () {
+        }
     };
 })();
 
-// Todo: Move to svgm when ready!
-function startMagic(){
-    $('.simple-circle').each(function(elem){
+// Todo: Move to the namespace when ready!
+function startMagic() {
+    $('.simple-circle').each(function (elem) {
         var translation = {
-            translateX: Math.abs(Math.random())*100+"vw",
-            translateY: Math.abs(Math.random())*100+"vh"
+            translateX: Math.abs(Math.random()) * 100 + "vw",
+            translateY: Math.abs(Math.random()) * 100 + "vh"
         };
         $(this).velocity(translation, {
-    /* Velocity's default options */
-    duration: 1000,
-    easing: "swing",
-    queue: "",
-    begin: undefined,
-    progress: undefined,
-    complete: undefined,
-    display: undefined,
-    visibility: undefined,
-    loop: false,
-    delay: false,
-    mobileHA: true
-}); 
+            /* Velocity's default options */
+            duration: 4000,
+            easing: "swing",
+            queue: "",
+            begin: undefined,
+            progress: undefined,
+            complete: undefined,
+            display: undefined,
+            visibility: undefined,
+            loop: false,
+            delay: false,
+            mobileHA: true
+        });
     });
 }
 
-function initializeMagic(){
+// Todo: Move to the namespace when ready!
+function initializeMagic() {
     'use strict';
-    var pattern =  $('.simple-circle').first();
-    for(var i = 0; i < 50; i++){
-         $('#svg-playground').append(pattern.clone());    
+    var pattern = $('.simple-circle').first();
+    for (var i = 0; i < 50; i++) {
+        $('#svg-playground').append(pattern.clone());
     }
 }
 
-function startLoop(){
+$(document).ready(function () {
     'use strict';
-    startMagic();
-    setTimeout(startLoop, 1000);
-}
-
-$(document).ready(function(){
     initializeMagic();
-    startLoop();
+    setInterval(startMagic, 4200);
 });
